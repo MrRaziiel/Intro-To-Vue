bd085d38e89282cb12cff8137058ad5f5efc9315
Ruben Amaro <ruben.amaro@gmail.com>
2022-12-12 19:10:17 +0000

    give more dynamic into the site
    update project with visibility buttons, add and remove object in cart

--- main.js
+++ main.js
@@ -1,10 +1,36 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Boots',
-            description : 'Blue',
+            cart: 0,
             image: './assets/images/socks_green.jpg',
-            inventory: 11
+            product: 'Sucks',
+            inventory: 10,
+            inStock: true,
+            details : ['50% cotton', '30% wool' , '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+            ],
+            sizes : ['XS', 'S', 'M', 'L', 'XL', 'XXL', 'G']
         }
+    },
+    methods: {
+        addToCart(){
+            this.cart +=1 
+        },
+        removeToCart(){
+            if (this.cart >0)
+            {
+                this.cart -=1 
+            }
+            
+        },
+        updateImage(varianteImage){
+
+                console.log("color");
+                this.image = varianteImage;
+            }
+
+
     }
 })
