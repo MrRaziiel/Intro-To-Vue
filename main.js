c1532fda9119fe869e5ac03aa72634eee80008a2
Ruben Amaro <ruben.amaro@gmail.com>
2022-12-13 18:18:43 +0000

    End of project
    added review to the form and some minor change

--- main.js
+++ main.js
@@ -1,49 +1,23 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
-            selectedVariant: 0,
-            product: 'Socks',
-            brand: 'Vue Mastery',
-            details : ['50% cotton', '30% wool' , '20% polyester'],
-            variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50, onSale: false },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity:0, onSale: true },
-            ],
-            sizes : ['XS', 'S', 'M', 'L', 'XL', 'XXL', 'G']
+            cart: [],
+            premium: true
         }
     },
     methods: {
-        addToCart(){
-            this.cart +=1 
-        },
-        removeToCart(){
-            if (this.cart >0)
-            {
-                this.cart -=1 
-            }
-            
-        },
-        updateVariant(index){
+        updateCart(id){
+           
+            this.cart.push(id)
 
-                console.log("color");
-                this.selectedVariant = index;
-            }
+        },
+        removeById(id) {
+            const index = this.cart.indexOf(id)
+                if (index > -1) {
+                    this.cart.splice(index, 1)
+                }
 
 
-    },
-    computed: {
-        title() {
-            var titleToShow = this.brand + ' ' + this.product
-            if (this.variants[this.selectedVariant].onSale)
-                titleToShow += " is on sale"
-            return titleToShow;
-        },
-        image(){
-            return this.variants[this.selectedVariant].image;
-        },
-        inStock(){
-            return this.variants[this.selectedVariant].quantity;
-        }
     }
+}
 })
