42326be6c0148e74d7973abb82bfbb8abb5b9fd8
Ruben Amaro <ruben.amaro@gmail.com>
2022-12-13 01:50:56 +0000

    better code in js
    removing validation in model
    adding validation at the view-model
    adding sales to title if isOnsale true

--- main.js
+++ main.js
@@ -2,14 +2,13 @@ const app = Vue.createApp({
     data() {
         return {
             cart: 0,
-            image: './assets/images/socks_green.jpg',
-            product: 'Sucks',
-            inventory: 10,
-            inStock: true,
+            selectedVariant: 0,
+            product: 'Socks',
+            brand: 'Vue Mastery',
             details : ['50% cotton', '30% wool' , '20% polyester'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50, onSale: false },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity:0, onSale: true },
             ],
             sizes : ['XS', 'S', 'M', 'L', 'XL', 'XXL', 'G']
         }
@@ -25,12 +24,26 @@ const app = Vue.createApp({
             }
             
         },
-        updateImage(varianteImage){
+        updateVariant(index){
 
                 console.log("color");
-                this.image = varianteImage;
+                this.selectedVariant = index;
             }
 
 
+    },
+    computed: {
+        title() {
+            var titleToShow = this.brand + ' ' + this.product
+            if (this.variants[this.selectedVariant].onSale)
+                titleToShow += " is on sale"
+            return titleToShow;
+        },
+        image(){
+            return this.variants[this.selectedVariant].image;
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].quantity;
+        }
     }
 })
